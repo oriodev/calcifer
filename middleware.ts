58a1ee7aefe9a58cb3f093373acfa7e349aed075
ori <93719767+oriodev@users.noreply.github.com>
2024-04-13 18:53:02 +0100

    finished middleware and routes

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,50 @@
 import authConfig from "./auth.config"
 import NextAuth from "next-auth"
 
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes
+} from '@/routes'
+
 const { auth } = NextAuth(authConfig)
 
+// @ts-ignore
 export default auth((req) => {
-  const isLoggedIn = !!req.auth
-  console.log('ROUTE: ', req.nextUrl.pathname)
-  console.log('IS LOGGED IN: ', isLoggedIn)
+  const { nextUrl } = req;
+
+  // returns true/false if logged in / if the route you're on is an api route
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+  // block api routes
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  // so routes like register and login, things u don't need to access if u r logged in already.
+  if (isAuthRoute) {
+
+    // so if we are logged in then we're gonna redirect.
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+    }
+
+    return null;
+  }
+
+  // if we r not logged in then we cannot be accessing public routes sorry
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL('/auth/login', nextUrl))
+  }
+
+  return null;
+
+
 })
 
 // Optionally, don't invoke Middleware on some paths
---
+++ routes.ts
@@ -0,0 +1,19 @@
+// routes that anyone can access w/o auth
+// type string[]
+export const publicRoutes = [
+  "/"
+]
+
+// routes that need auth to access
+// logged in users will be redirected to /settings
+// type string[]
+export const authRoutes = [
+  '/auth/login',
+  '/auth/register'
+]
+
+// prefix for all the auth api routes
+export const apiAuthPrefix = "/api/auth"
+
+// default redirect after logging in
+export const DEFAULT_LOGIN_REDIRECT = '/settings'
