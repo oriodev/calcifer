7205a736e64192d16b2acb17be6124cd41cb814c
ori <93719767+oriodev@users.noreply.github.com>
2024-04-27 20:31:38 +0100

    messing with socials sign in redirects

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,14 +3,16 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 import { signIn } from 'next-auth/react'
+import { currentUser } from './auth';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 
-export const socialSignIn = (provider: 'github' | 'google') => {
+export const socialSignIn = async (provider: 'github' | 'google') => {
+
   signIn(provider, {
-    callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    callbackUrl: '/auth/onboarding'
   })
 }
